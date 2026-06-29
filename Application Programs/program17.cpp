c29b87f1c41ae3ba5cf2b597d23a5b02a8cafc14
Neha27-G <niagupta8@gmail.com>
2026-06-29 22:45:31 +0530

    sequence printing

---
+++ Application Programs/program17.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+//Sequence
+int main()
+{
+    std::cout<<"Jay Ganesh...\n";
+    std::cout<<"Jay Ganesh...\n";
+    std::cout<<"Jay Ganesh...\n";
+    std::cout<<"Jay Ganesh...\n";
+    
+    return 0;
+}
