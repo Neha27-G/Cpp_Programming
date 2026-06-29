8c0494c2f286cfdfbef691b2aab691c6b15ec36f
Neha27-G <niagupta8@gmail.com>
2026-06-29 22:44:35 +0530

    program performing if else condition

---
+++ Application Programs/program12.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int CalculateTicketPrice(int iAge)
+{
+    //Updater
+    if(iAge < 0)
+    {
+        iAge = -iAge;
+    }
+
+    if(iAge >= 0  &&  iAge <=5)
+    {
+        return 0;
+    }
+    else if(iAge >= 6  &&  iAge <=18)
+    {
+        return 500;
+    }
+    else if(iAge >= 19  &&  iAge <=50)
+    {
+        return 900;
+    }
+    else
+    {
+        return 400;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Please Enter the Age to Calculate Ticket Price: ";
+    cin>>iValue;
+
+    iRet = CalculateTicketPrice(iValue);
+    cout<<"Your Ticket price will be "<<iRet<<" rupess\n";
+
+    return 0;
+}
---
+++ Application Programs/program13.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int CalculateTicketPrice(int iAge)
+{
+    //Filter
+    if(iAge < 0)
+    {
+        printf("Invalid Input.\n");
+        return 0;
+    }
+
+    if(iAge >= 0  &&  iAge <=5)
+    {
+        return 0;
+    }
+    else if(iAge >= 6  &&  iAge <=18)
+    {
+        return 500;
+    }
+    else if(iAge >= 19  &&  iAge <=50)
+    {
+        return 900;
+    }
+    else
+    {
+        return 400;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Please Enter the Age to Calculate Ticket Price: ";
+    cin>>iValue;
+
+    iRet = CalculateTicketPrice(iValue);
+    cout<<"Your Ticket price will be "<<iRet<<" rupess\n";
+
+    return 0;
+}
