00b32dcec6867b0b02d894f0356558ae2fdcd52b
Neha27-G <niagupta8@gmail.com>
2026-07-09 23:46:13 +0530

    program to check whether the number is prime number or not

---
+++ Application Programs/program48.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo <= 1)
+    {
+        return false;
+    }
+
+    for(iCnt = 2; iCnt <=iNo/2; iCnt++)
+    {
+        if((iNo % iCnt) ==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    cout<<"Enter the Number :";
+    cin>>iValue;
+
+    bRet = CheckPrime(iValue);
+
+    if(bRet == true)
+    {
+        cout<<"It is Prime Number\n";
+    }
+    else
+    {
+        cout<<"It is Not Prime Number\n";
+    }
+    return 0;
+}
