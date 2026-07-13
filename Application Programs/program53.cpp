140ce7a10c912db440aacb2acee66f5307792a87
Neha27-G <niagupta8@gmail.com>
2026-07-13 22:23:28 +0530

    program to display n Numbers

---
+++ Application Programs/program53.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    while (iCnt <=5)
+    {
+        cout<<iCnt<<"  ";
+        iCnt++;
+    }
+    cout<<"\n";
+    
+    return 0;
+}
---
+++ Application Programs/program54.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 5;
+
+    while (iCnt >=1)
+    {
+        cout<<iCnt<<"  ";
+        iCnt--;
+    }
+    cout<<"\n";
+    
+    return 0;
+}
