6f103e1515fea0a748b0357e1ff8df3952e94bb6
Neha27-G <niagupta8@gmail.com>
2026-07-19 22:38:31 +0530

    program to count the Digit 7 from the given number

---
+++ Application Programs/program64.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int CountDigits(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    if(iNo == 0)
+    {
+        return 1;
+    }
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit == 7)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter The Number :";
+    cin>>iValue;
+
+    iRet = CountDigits(iValue);
+    cout<<"Number of Digits are :"<<iRet;
+
+    return 0;
+}
