a83588f211a7cc17778d1a93cb7cd90172a87a85
Neha27-G <niagupta8@gmail.com>
2026-07-19 22:39:48 +0530

    program to count the Odd Digits from the number

---
+++ Application Programs/program66.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int CountOddDigits(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    if(iNo == 0)
+    {
+        return 1;
+    }
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if((iDigit % 2) != 0)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter The Number :";
+    cin>>iValue;
+
+    iRet = CountOddDigits(iValue);
+    cout<<"Number of Even Digits are :"<<iRet;
+
+    return 0;
+}
