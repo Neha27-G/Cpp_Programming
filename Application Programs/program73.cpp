f242965f8960c8f46abe76c4f533b719a82dbc30
Neha27-G <niagupta8@gmail.com>
2026-07-25 23:58:05 +0530

    program based on array

---
+++ Application Programs/program73.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int Arr[] = {10,20,30,40,50};
+
+    cout<<Arr<<endl;
+    cout<<&Arr<<endl;
+    cout<<&Arr[0]<<endl;
+
+    return 0;
+}
