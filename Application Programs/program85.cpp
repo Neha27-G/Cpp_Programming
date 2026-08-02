430ae3718088e923efaa1b580aced0c337f66501
Neha27-G <niagupta8@gmail.com>
2026-08-02 23:54:59 +0530

    Program demonstrating Array and Pointer

---
+++ Application Programs/program85.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int Display(int iptr[])
+{
+    printf("%d\n",*iptr);
+    iptr++;
+
+    printf("%d\n",*iptr);
+    iptr++;
+
+    printf("%d\n",*iptr);
+
+}
+
+int main()
+{
+    int Arr[5] = {10,20,30,40,50};
+
+    Display(Arr);
+    
+    return 0;
+}
