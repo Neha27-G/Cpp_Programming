98d91318f5c5e43f2875153353dbd326ab62bc24
Neha27-G <niagupta8@gmail.com>
2026-08-03 23:24:22 +0530

    program performing an array

---
+++ Application Programs/program91.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void Display(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        cout<<Arr[iCnt]<<"\n";
+    }
+}
+
+int main()
+{
+    int iLength = 4;
+    
+    int Brr[iLength];
+
+    Brr[0] = 10;
+    Brr[1] = 20;
+    Brr[2] = 30;
+    Brr[3] = 40;
+
+    Display(Brr,iLength);
+    
+    return 0;
+}
