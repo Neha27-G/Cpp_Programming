2c4d11ab3f75cac2d733fcec799f1c852ff07f99
Neha27-G <niagupta8@gmail.com>
2026-08-06 23:33:25 +0530

    program on Summation of numbers in Array

---
+++ Application Programs/program96.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int Summation(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    for(iCnt = 0; iCnt< iSize; iCnt++)
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+    return iSum;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0, iCnt = 0, iRet = 0;
+
+    cout<<"Enter number of Elements :\n";
+    cin>>iLength;
+
+    Brr = new int[iLength];
+
+    cout<<"Enter the elements  :\n";
+
+    for (iCnt = 0; iCnt < iLength ; iCnt++)
+    {
+        cin>>Brr[iCnt];
+    }
+
+    iRet = Summation(Brr, iLength);
+    cout<<"Addition of all Elements are : \n"<<iRet;
+
+    delete[] Brr;
+    
+    return 0;
+}
